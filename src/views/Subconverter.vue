abd4ce5bb6ccfbd911da21e65539ada4a8af1ed8
qqxu223 <157944454+qqxu223@users.noreply.github.com>
2025-07-02 16:32:21 +0800

    更新 Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,7 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
-      "自用专属后端": "https://subapi.caomu.cloudns.ch",// 为您的后端取名
+      "自用专属后端": "https://subapi.caomu.hidns.co",// 为您的后端取名
       "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
       "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
       "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -348,7 +348,7 @@ export default {
       "sub-web作者提供": "https://api.wcc.best",
   },
   backendOptions: [
-      { value: "https://subapi.caomu.cloudns.ch" }, // 添加您的后端地址
+      { value: "https://subapi.caomu.hidns.co" }, // 添加您的后端地址
       { value: "https://subapi.cmliussss.net" },
       { value: "https://subapi.fxxk.dedyn.io" },
       { value: "https://url.v1.mk" },
@@ -792,7 +792,7 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://subapi.caomu.cloudns.ch" : this.getUrlParam(),
+        customBackend: this.getUrlParam() == "" ? "https://subapi.caomu.hidns.co" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
         remoteConfig: "https://raw.githubusercontent.com/liandu2024/clash/refs/heads/main/Clash-LIAN.ini",
         excludeRemarks: "",
